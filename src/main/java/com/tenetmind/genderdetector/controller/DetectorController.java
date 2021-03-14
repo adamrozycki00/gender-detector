049af0f53a32ee803dd75e0a9657186e4628f81d
adamrozycki00 <adamrozycki00@gmail.com>
2021-03-14 11:20:38 +0100

    Add DetectorController

---
+++ src/main/java/com/tenetmind/genderdetector/controller/DetectorController.java
@@ -0,0 +1,35 @@
+package com.tenetmind.genderdetector.controller;
+
+import com.tenetmind.genderdetector.service.DetectorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+public class DetectorController {
+
+    @Autowired
+    private DetectorService detectorService;
+
+    @PostMapping(value = "/detect", consumes = APPLICATION_JSON_VALUE)
+    public String detectGender(@RequestBody RequestDto requestDto) {
+        return detectorService.detectGender(requestDto.getSourceStringToCheck(), requestDto.getDetectorVariantName());
+    }
+
+    @GetMapping(value = "/tokens")
+    public List<String> getTokens(@RequestParam String gender, @RequestParam long page, @RequestParam long size) {
+        try {
+            return detectorService.getTokens(gender, page, size);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ArrayList<>();
+    }
+
+}
